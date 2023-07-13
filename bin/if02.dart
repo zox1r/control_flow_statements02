a211d7e2cfbab99ee1e1a767f6761fec303d55f7
zox1r <qarshiyevzoxir@gmail.com>
2023-07-13 10:24:50 +0200

    bin/if02.dart

--- bin/if02.dart
+++ bin/if02.dart
@@ -8,5 +8,21 @@ Args:
 Returns:
     int: return answer.
 */
+int func(int a, b, c){
+         if(a < b){
+        if(a < c){
+         return a;
+        }else {
+           return c;
+        }
+    } else {
+        if(b < c){
+        return b;
+    } else{
+        return c;
+    }
+}
+}
 void main() {
+    print(func(4, 2, 3));
 }
