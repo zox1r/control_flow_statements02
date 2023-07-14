ffff0314e18b4d15dedc2a67ac6dcaab9308754d
zox1r <qarshiyevzoxir@gmail.com>
2023-07-14 15:17:54 +0200

    bin/if05.dart

--- bin/if05.dart
+++ bin/if05.dart
@@ -6,6 +6,28 @@ Args:
 Returns:
     int: return answer.
 */
-
+int func(int a){
+  int x = a ~/ 1000;
+ int y = a ~/ 100 % 10;
+ int q = a % 100 ~/ 10;
+  int z = a % 10;
+ if(x > y){
+    if(x > q){
+        if(x > z){
+            return x;
+        }
+    }
+    }   if(y > x){
+        return y;
+   } else{
+ if(q > z){
+    return q;
+} else{
+    return z;
+}
+   }
+    
+}
 void main() {
+    print(func(5876));
 }
